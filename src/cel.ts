08b7e7d664c701ad47a69146df0652e264b15981
Alex <lepsalex@gmail.com>
2020-05-12 16:28:20 -0400

    check engine endpoint working

---
+++ src/cel.ts
@@ -0,0 +1,34 @@
+type Run = {
+  runName: string;
+  state: string;
+  completeTime: string;
+};
+
+export const cel = (runs: [Run]) => {
+  const limit = process.env.CEL_LIMIT || 3;
+  const rangeInDays = process.env.CEL_RANGE_DAYS || 3;
+
+  const now = new Date();
+
+  const celRuns = runs.map((run) => {
+    const completeTime = new Date(parseInt(run["completeTime"]));
+    const delta = now.getTime() - completeTime.getTime();
+    const deltaDays = Math.floor(delta / 1000 / 60 / 60 / 24);
+
+    return {
+      ...run,
+      completeTime: completeTime,
+      cel: deltaDays < rangeInDays ? true : false,
+    };
+  });
+
+  const celCount = celRuns.reduce((acc, curr) => {
+    return curr.cel === true ? acc + 1 : acc;
+  }, 0);
+
+  return {
+    cel: celCount >= limit,
+    count: celCount,
+    runs: celRuns,
+  };
+};
--- src/handlers.ts
+++ src/handlers.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
 import { errorReport } from "./repository/wes";
+import { cel } from './cel';
 
 export const rootHandler = (_req: Request, res: Response) => {
   return res.send("API is working 🤓");
 };
 
 export const celHandler = async (_req: Request, res: Response) => {
-    const data = await errorReport(10);
-    return res.json(data["runs"]);
+  const data = await errorReport(10);
+  res.json(cel(data["runs"]));
 };
